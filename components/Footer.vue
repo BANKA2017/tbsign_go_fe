4f0aaf744944027a27f6e9e82ccbf50f369bcac9
BANKA2017 <17263253+BANKA2017@users.noreply.github.com>
2024-07-04 01:51:09 +0800

    fix: footer layout for private instance

--- components/Footer.vue
+++ components/Footer.vue
@@ -2,7 +2,7 @@
     <footer class="text-end py-10 text-sm">
         <section class="text-gray-900 dark:text-gray-100 font-sans mb-1 rounded-full flex justify-end">
             <div v-if="config.public.NUXT_BASE_PATH" role="button" class="inline-block">
-                <span class="border border-sky-500 rounded-r-full px-2">Private</span>
+                <span class="border border-sky-500 rounded-full px-2">Private</span>
             </div>
             <div v-else role="button" class="inline-block" :title="basePath" @click="showMore = !showMore">
                 <span class="bg-sky-500 border border-sky-500 text-gray-100 rounded-l-full px-2">API</span>
