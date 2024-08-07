0ed04223887a45121b1b91a9306f843eff9642db
BANKA2017 <17263253+BANKA2017@users.noreply.github.com>
2024-08-08 03:12:04 +0800

    try: hide (NOT FIX!) hydration mismatches

--- pages/reset_password.vue
+++ pages/reset_password.vue
@@ -64,19 +64,21 @@ const sendRequest = (e: Event) => {
 <template>
     <NuxtLayout name="tbsign">
         <frame-work>
-            <div class="flex justify-center">
-                <form class="rounded-2xl p-5 flex grow flex-col gap-2 max-w-[32em]">
-                    <span v-if="runtimeConfig.public.NUXT_BASE_PATH === ''" class="rounded-2xl bg-gray-200 dark:bg-gray-800 p-5 mb-10" v-show="step === 0">正在找回 {{ basePath }} 的密码</span>
-                    <div class="rounded-2xl bg-gray-200 dark:bg-gray-800 p-5 mb-2" v-show="step === 1 || step === 2">{{ step === 1 ? '如果此邮箱存在，您将会收到一封包含六位数字验证码的邮件' : '密码修改成功 🎉' }}</div>
-                    <label for="email">注册邮箱</label>
-                    <input class="bg-gray-100 dark:bg-gray-900 rounded-xl" id="email" type="email" placeholder="注册邮箱" v-model="email" />
-                    <label v-show="step > 0" for="invite-code">验证码</label>
-                    <input v-show="step > 0" class="bg-gray-100 dark:bg-gray-900 rounded-xl" id="invite-code" type="text" placeholder="六位验证码" v-model="code" />
-                    <label v-show="step > 0 && code" for="new-password">密码</label>
-                    <input v-show="step > 0 && code" autocomplete="new-password" class="bg-gray-100 dark:bg-gray-900 rounded-xl" id="new-password" type="password" placeholder="密码" v-model="password" />
-                    <input v-if="step < 2" type="submit" class="text-gray-100 rounded-xl mt-3 px-3 py-1 bg-sky-500 hover:bg-sky-400 dark:hover:bg-sky-600" @click="sendRequest" value="提交" />
-                </form>
-            </div>
+            <ClientOnly>
+                <div class="flex justify-center">
+                    <form class="rounded-2xl p-5 flex grow flex-col gap-2 max-w-[32em]">
+                        <span v-if="runtimeConfig.public.NUXT_BASE_PATH === ''" class="rounded-2xl bg-gray-200 dark:bg-gray-800 p-5 mb-10" v-show="step === 0">正在找回 {{ basePath }} 的密码</span>
+                        <div class="rounded-2xl bg-gray-200 dark:bg-gray-800 p-5 mb-2" v-show="step === 1 || step === 2">{{ step === 1 ? '如果此邮箱存在，您将会收到一封包含六位数字验证码的邮件' : '密码修改成功 🎉' }}</div>
+                        <label for="email">注册邮箱</label>
+                        <input class="bg-gray-100 dark:bg-gray-900 rounded-xl" id="email" type="email" placeholder="注册邮箱" v-model="email" />
+                        <label v-show="step > 0" for="invite-code">验证码</label>
+                        <input v-show="step > 0" class="bg-gray-100 dark:bg-gray-900 rounded-xl" id="invite-code" type="text" placeholder="六位验证码" v-model="code" />
+                        <label v-show="step > 0 && code" for="new-password">密码</label>
+                        <input v-show="step > 0 && code" autocomplete="new-password" class="bg-gray-100 dark:bg-gray-900 rounded-xl" id="new-password" type="password" placeholder="密码" v-model="password" />
+                        <input v-if="step < 2" type="submit" class="text-gray-100 rounded-xl mt-3 px-3 py-1 bg-sky-500 hover:bg-sky-400 dark:hover:bg-sky-600" @click="sendRequest" value="提交" />
+                    </form>
+                </div>
+            </ClientOnly>
         </frame-work>
     </NuxtLayout>
 </template>
