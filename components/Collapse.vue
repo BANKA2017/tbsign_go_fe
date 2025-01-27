68e1ac192924fa40d99c34a121aa89faae0b4d00
BANKA2017 <17263253+BANKA2017@users.noreply.github.com>
2025-01-27 19:17:08 +0800

    update: add component Collapse

---
+++ components/Collapse.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="my-2 px-3 py-1 bg-gray-200 dark:bg-gray-800 rounded-xl transition-colors">
+        <h3 role="button" :class="'flex justify-between text-lg py-2 px-2 -mx-2 sticky top-0 ' + (expand ? 'bg-gray-200 dark:bg-gray-800' : '')" @click="expand = !expand">
+            <div>
+                <slot>展开</slot>
+            </div>
+
+            <div role="button" class="rounded-full h-4 text-gray-900 dark:text-gray-100 transition-colors my-1" title="展开设置列表" aria-label="展开设置列表">
+                <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" fill="currentColor" class="w-5 h-5" viewBox="0 0 16 16">
+                    <path
+                        v-if="expand"
+                        fill="currentColor"
+                        fill-rule="evenodd"
+                        d="M1 8a.5.5 0 0 1 .5-.5h13a.5.5 0 0 1 0 1h-13A.5.5 0 0 1 1 8m7-8a.5.5 0 0 1 .5.5v3.793l1.146-1.147a.5.5 0 0 1 .708.708l-2 2a.5.5 0 0 1-.708 0l-2-2a.5.5 0 1 1 .708-.708L7.5 4.293V.5A.5.5 0 0 1 8 0m-.5 11.707l-1.146 1.147a.5.5 0 0 1-.708-.708l2-2a.5.5 0 0 1 .708 0l2 2a.5.5 0 0 1-.708.708L8.5 11.707V15.5a.5.5 0 0 1-1 0z"
+                    />
+                    <path
+                        v-else
+                        fill="currentColor"
+                        fill-rule="evenodd"
+                        d="M1 8a.5.5 0 0 1 .5-.5h13a.5.5 0 0 1 0 1h-13A.5.5 0 0 1 1 8M7.646.146a.5.5 0 0 1 .708 0l2 2a.5.5 0 0 1-.708.708L8.5 1.707V5.5a.5.5 0 0 1-1 0V1.707L6.354 2.854a.5.5 0 1 1-.708-.708zM8 10a.5.5 0 0 1 .5.5v3.793l1.146-1.147a.5.5 0 0 1 .708.708l-2 2a.5.5 0 0 1-.708 0l-2-2a.5.5 0 0 1 .708-.708L7.5 14.293V10.5A.5.5 0 0 1 8 10"
+                    />
+                </svg>
+            </div>
+        </h3>
+        <div :class="{ 'list-in': expand, 'list-out': !expand, 'mb-3': expand }" :style="{ 'max-height': expand ? '100vh' : 0 }">
+            <hr class="border-gray-400 dark:border-gray-600 my-1" />
+            <slot name="container">这里什么都没有~</slot>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+const expand = ref<boolean>(false)
+</script>
--- pages/system_admin.vue
+++ pages/system_admin.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import Collapse from '~/components/Collapse.vue'
 import UpdateSystemItem from '~/components/UpdateSystemItem.vue'
 import { Notice, Request } from '~/share/Tools'
 
@@ -92,22 +93,18 @@ const resignStatus = computed(() => {
 const settingsGroup = ref({
     system: {
         name: '系统',
-        display: false,
         data: { ann: '公告', system_url: '地址' } //, system_name: '网站名称', system_keywords: '关键词', system_description: '简介' }
     },
     account: {
         name: '账号',
-        display: false,
         data: { enable_reg: '开启注册', yr_reg: '邀请码 (留空代表无需邀请码)', cktime: 'Session 有效期 (设置后签发的 Token 才生效)' }
     },
     backup: {
         name: '备份',
-        display: false,
         data: { go_export_personal_data: '允许导出个人数据', go_import_personal_data: '允许导入个人数据' }
     },
     checkin: {
         name: '签到',
-        display: false,
         data: {
             sign_mode: '签到模式 (TODO)',
             sign_hour: '下个整点签到 (-1 为 0 时开始签到，以此类推)',
@@ -119,17 +116,14 @@ const settingsGroup = ref({
     },
     mail: {
         name: '邮件',
-        display: false,
         data: { mail_name: '发件人邮箱', mail_yourname: '发件人名称', mail_host: 'SMTP服务器地址', mail_port: 'SMTP服务器端口', mail_secure: '加密方式 (TODO)', mail_auth: '需要身份验证', mail_smtpname: 'SMTP用户名', mail_smtppw: 'SMTP密码' }
     },
     push: {
         name: '推送',
-        display: false,
         data: { go_bark_addr: 'Bark 推送地址', go_ntfy_addr: 'ntfy 推送地址', go_pushdeer_addr: 'PushDeer 推送地址' }
     },
     plugin: {
         name: '插件',
-        display: false,
         data: {}
     }
 })
@@ -504,31 +498,11 @@ onMounted(() => {
                 <p class="text-sm">如果不知道要填什么，请保持原样</p>
             </div>
             <form autocomplete="off">
-                <div class="my-2 px-3 py-1 bg-gray-200 dark:bg-gray-800 rounded-xl transition-colors" v-for="_set in settingsGroup" :key="_set.name">
-                    <h3 role="button" :class="'flex justify-between text-lg py-2 px-2 -mx-2 sticky top-0 ' + (_set.display ? 'bg-gray-200 dark:bg-gray-800' : '')" @click="_set.display = !_set.display">
-                        <span>
-                            {{ _set.name }}
-                        </span>
-
-                        <span class="rounded-full h-4 text-gray-900 dark:text-gray-100 transition-colors my-1" title="展开设置列表" aria-label="展开设置列表">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" fill="currentColor" class="w-5 h-5" viewBox="0 0 16 16">
-                                <path
-                                    v-if="_set.display"
-                                    fill="currentColor"
-                                    fill-rule="evenodd"
-                                    d="M1 8a.5.5 0 0 1 .5-.5h13a.5.5 0 0 1 0 1h-13A.5.5 0 0 1 1 8m7-8a.5.5 0 0 1 .5.5v3.793l1.146-1.147a.5.5 0 0 1 .708.708l-2 2a.5.5 0 0 1-.708 0l-2-2a.5.5 0 1 1 .708-.708L7.5 4.293V.5A.5.5 0 0 1 8 0m-.5 11.707l-1.146 1.147a.5.5 0 0 1-.708-.708l2-2a.5.5 0 0 1 .708 0l2 2a.5.5 0 0 1-.708.708L8.5 11.707V15.5a.5.5 0 0 1-1 0z"
-                                />
-                                <path
-                                    v-else
-                                    fill="currentColor"
-                                    fill-rule="evenodd"
-                                    d="M1 8a.5.5 0 0 1 .5-.5h13a.5.5 0 0 1 0 1h-13A.5.5 0 0 1 1 8M7.646.146a.5.5 0 0 1 .708 0l2 2a.5.5 0 0 1-.708.708L8.5 1.707V5.5a.5.5 0 0 1-1 0V1.707L6.354 2.854a.5.5 0 1 1-.708-.708zM8 10a.5.5 0 0 1 .5.5v3.793l1.146-1.147a.5.5 0 0 1 .708.708l-2 2a.5.5 0 0 1-.708 0l-2-2a.5.5 0 0 1 .708-.708L7.5 14.293V10.5A.5.5 0 0 1 8 10"
-                                />
-                            </svg>
-                        </span>
-                    </h3>
-                    <div :class="{ 'list-in': _set.display, 'list-out': !_set.display, 'mb-3': _set.display }" :style="{ 'max-height': _set.display ? '100vh' : 0 }">
-                        <hr class="border-gray-400 dark:border-gray-600 my-1" />
+                <Collapse v-for="_set in settingsGroup" :key="_set.name">
+                    <template #default>
+                        {{ _set.name }}
+                    </template>
+                    <template #container>
                         <template v-for="(name, key) in _set.data" :key="key">
                             <label :for="'input-' + key" class="block text-sm font-medium mb-1 mt-3">{{ name }}</label>
                             <textarea
@@ -667,8 +641,8 @@ onMounted(() => {
                         >
                             发送测试邮件
                         </button>
-                    </div>
-                </div>
+                    </template>
+                </Collapse>
                 <input type="submit" role="button" class="text-gray-100 text-lg bg-sky-500 hover:bg-sky-600 dark:hover:bg-sky-400 rounded-xl ml-3 px-5 py-1 transition-colors" @click="SaveSettings" value="保存" />
             </form>
         </div>
